5ab8be6edf56a4a7a264725d2fdf8bf2c9b230d7
ejortiz17 <eliud.j.ortiz@gmail.com>
2019-04-24 23:23:35 -0700

    added append to linked list implementation and print statements

--- linked_lists/linkedlist.py
+++ linked_lists/linkedlist.py
@@ -1,3 +1,18 @@
 class LinkedList:
     def __init__(self, head_node):
         self.headNode = head_node
+
+    def append(self, node):
+        if node.next is not None:
+            return
+        current_node = self.headNode
+        while current_node.next is not None:
+            current_node = current_node.next
+        current_node.next = node
+
+    # Print the linked list
+    def listprint(self):
+        node = self.headNode
+        while node is not None:
+            print(node.val)
+            node = node.next
---
+++ linked_lists/main.py
@@ -0,0 +1,12 @@
+from linkedlist import LinkedList
+from node import Node
+
+node_0 = Node(0)
+node_1 = Node(1)
+node_2 = Node(2)
+
+linked_list = LinkedList(node_0)
+linked_list.append(node_1)
+linked_list.append(node_2)
+
+linked_list.listprint()
