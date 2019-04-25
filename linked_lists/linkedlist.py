28e727a3fe5e3a0a938f08c66bf63a8c4f33a36f
ejortiz17 <eliud.j.ortiz@gmail.com>
2019-04-25 00:09:48 -0700

    added remove and insert methods to linked list

--- linked_lists/linkedlist.py
+++ linked_lists/linkedlist.py
@@ -10,6 +10,24 @@ class LinkedList:
             current_node = current_node.next
         current_node.next = node
 
+    def prepend(self, node):
+        temp_node = self.headNode
+        self.headNode = node
+        node.next = temp_node
+
+    def insert(self, node, existing_node):
+        right_node = existing_node.next
+        existing_node.next = node
+        node.next = right_node
+
+    def remove(self, node_to_remove):
+        current_node = self.headNode
+        while current_node.next is not node_to_remove:
+            current_node = current_node.next
+        right_node = node_to_remove.next
+        current_node.next = right_node
+        del node_to_remove
+        
     # Print the linked list
     def listprint(self):
         node = self.headNode
--- linked_lists/main.py
+++ linked_lists/main.py
@@ -4,9 +4,13 @@ from node import Node
 node_0 = Node(0)
 node_1 = Node(1)
 node_2 = Node(2)
-
+node_3 = Node(-1)
+node_4 = Node(1.5)
 linked_list = LinkedList(node_0)
 linked_list.append(node_1)
 linked_list.append(node_2)
 
+linked_list.prepend(node_3)
+linked_list.insert(node_4, node_1)
+linked_list.remove(node_0)
 linked_list.listprint()
